28dc47a4de15c1d95fbec16493bd4b9541ab8058
srikanthsaireddy <52093598+srikanthsaireddy@users.noreply.github.com>
2021-07-18 18:13:16 +0530

    Create palindromornot.java

---
+++ palindromornot.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+  int r,sum=0,temp;    
+  
+  Scanner s=new Scanner(System.in);
+  int n=s.nextInt();
+  temp=n;    
+  while(n>0){    
+   r=n%10;  //getting remainder  
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }    
+  if(temp==sum)    
+   System.out.println("palindrome number ");    
+  else    
+   System.out.println("not palindrome");  
+	}
+}
